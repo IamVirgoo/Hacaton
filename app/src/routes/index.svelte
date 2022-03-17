27c373f6adcbd5e34ae0615698e32832eb23e98e
Karen Khachatryan <argentum7732004@gmail.com>
2022-03-17 23:53:30 +0300

    file edit + header

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,59 +1,33 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
-
 <svelte:head>
 	<title>Home</title>
+	<link rel="stylesheet" href="src/css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div class="container">
+		<header>
+			<div class="content">
+				<img src="/static/logo.svg" alt="">
+				<h1 class="header-title">CPP Segmentation<br>error</h1>	
+				<div class="nav">
+					<!-- svelte-ignore a11y-invalid-attribute -->
+					<a href="">Team</a>
+					<!-- svelte-ignore a11y-invalid-attribute -->
+					<a href="">Get Started</a>
+				</div>
+			</div>
+			<hr>
+		</header>
+	</div>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<section>
+	<div class="conteiner">
+		<div class="hight-content"></div>
+	</div>
+</section>
+<section>
+	<div class="container">
+		<div class="team-content"></div>
+	</div>
+</section>
