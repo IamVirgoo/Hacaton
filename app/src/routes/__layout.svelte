ea0f8c0b952344705d2c4a072c89dd13193b6ee1
MaskedTrench <>
2022-03-19 00:06:51 +0300

    Refactored

---
+++ app/src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<header>
+    <container class="flex space--between align-center">
+        <a class="nav--logo-wrap" href="/">
+            <img class="nav--logo" src="src/img/logo.png" alt="">
+        </a>
+        <p>CPP segmentation error</p>
+        <div class="nav--links">
+            <a href="/">Text</a>
+            <a href="/">Text</a>
+            <a href="/">Text</a>
+            <a href="/">Text</a>
+        </div>
+    </container>
+</header>
+
+<slot></slot>
+
+<footer></footer>
+
+<script>
+
+</script>
